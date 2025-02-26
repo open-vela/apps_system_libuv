9f3dae0d6c67d697f62152bcfd928c503817a986
yangsong8 <yangsong8@xiaomi.com>
2025-02-26 19:44:39 +0800

    libuv: Support for disabling float when compiling
    Signed-off-by: yangsong8 <yangsong8@xiaomi.com>

--- src/unix/sysinfo-loadavg.c
+++ src/unix/sysinfo-loadavg.c
@@ -30,7 +30,7 @@ void uv_loadavg(double avg[3]) {
 
   if (sysinfo(&info) < 0) return;
 
-  avg[0] = (double) info.loads[0] / 65536.0;
-  avg[1] = (double) info.loads[1] / 65536.0;
-  avg[2] = (double) info.loads[2] / 65536.0;
+  avg[0] = (double) info.loads[0] / 65536;
+  avg[1] = (double) info.loads[1] / 65536;
+  avg[2] = (double) info.loads[2] / 65536;
 }
